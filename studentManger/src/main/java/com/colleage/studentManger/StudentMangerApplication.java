1e3b3c6612a301595c2ca0ff364f608f8f76cb59
alexgim <alexgim961101@gmail.com>
2023-02-03 15:29:35 +0900

    Project : studentManger with regex

---
+++ studentManger/src/main/java/com/colleage/studentManger/StudentMangerApplication.java
@@ -0,0 +1,13 @@
+package com.colleage.studentManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMangerApplication.class, args);
+	}
+
+}
